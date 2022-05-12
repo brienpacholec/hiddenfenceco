61c672d0b717b404dcbb3d967962bdf476565935
Brien Pacholec <33663968+brienpacholec@users.noreply.github.com>
2022-05-12 16:41:58 -0400

    updated main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vuetify from "vuetify";
 import DefaultLayout from "~/layouts/Default.vue";
 
 import "vuetify/dist/vuetify.min.css";
-import '@mdi/font/css/materialdesignicons.css'
 
 import colors from "vuetify/lib/util/colors";
 
