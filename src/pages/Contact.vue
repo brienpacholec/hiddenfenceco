1768f103868fc590ca97de454daff55f1c8dcd40
Brien Pacholec <33663968+brienpacholec@users.noreply.github.com>
2022-06-23 19:51:42 -0400

    updated contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,17 +2,25 @@
   <Layout>
     <v-container>
       <h1>Contact Us</h1>
-      
-      <span v-html="this.$page.body.edges[0].node.content"></span>
 
+      <span v-html="this.$page.body.edges[0].node.content"></span>
 
       <p>Send a message, and we'll reply as soon as possible!</p>
 
-      <v-form ref="form" v-model="valid" lazy-validation name="contact" data-netlify="true" method="POST">
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+        name="Contact"
+        data-netlify="true"
+        method="POST"
+        v-on:submit.prevent="handleSubmit"
+      >
         <v-text-field
-          v-model="name"
-          :rules="nameRules"
+          v-model="formData.name"
+          name="name"
           label="Name"
+          :rules="nameRules"
           required
           autocomplete
         ></v-text-field>
@@ -23,9 +31,10 @@
             md="6"
           >
             <v-text-field
-              v-model="email"
+              v-model="formData.email"
+              name="email"
+              label="Email"
               :rules="emailRules"
-              label="E-mail"
               required
               autocomplete
             ></v-text-field>
@@ -36,9 +45,10 @@
             md="6"
           >
             <v-text-field
-              v-model="subjcet"
-              :rules="subjectRules"
+              v-model="formData.subject"
+              name="subject"
               label="Subject"
+              :rules="subjectRules"
               required
               autocomplete
             ></v-text-field>
@@ -46,22 +56,29 @@
         </v-row>
 
         <v-textarea
-          v-model="message"
+          v-model="formData.message"
+          name="message"
           label="Message"
           rows="1"
           row-height="15"
           required
         ></v-textarea>
         <v-btn
-          :disabled="!valid"
           color="success"
-          @click="validate"
+          type="submit"
           required
+          :disabled="!valid"
+          @click="validate"
         >
           Submit
         </v-btn>
       </v-form>
     </v-container>
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ snackbarText }}
+    </v-snackbar>
   </Layout>
 </template>
 
@@ -81,42 +98,65 @@
 <script>
 export default {
   data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 30) || 'Name must be less than 30 characters',
-        v => (v && v.length > 2) || 'Name must be greater than 2 characters'
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      subject: '',
-      subjectRules: [
-        v => !!v || 'Subject is required',
-        v => (v && v.length <= 30) || 'Name must be less than 30 characters',
-        v => (v && v.length > 2) || 'Subject must be greater than 2 characters'
-      ],
-      message: '',
-      
+    valid: true,
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 30) || 'Name must be less than 30 characters',
+      v => (v && v.length > 2) || 'Name must be greater than 2 characters'
+    ],
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    subjectRules: [
+      v => !!v || 'Subject is required',
+      v => (v && v.length <= 30) || 'Name must be less than 30 characters',
+      v => (v && v.length > 2) || 'Subject must be greater than 2 characters'
+    ],
+    formData: {},
+    snackbar: false,
+    snackbarText: null,
   }),
   metaInfo: {
     title: 'Contact'
   },
   methods: {
-      validate () {
-        if (this.$refs.form.validate()) {
-          this.snackbar = true
-        }
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+      }
     },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    },
+    encode(data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    },
+    handleSubmit(e) {
+      console.log(e.target.getAttribute('name'))
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+        .then(() => {
+          this.snackbar = true
+          this.snackbarText = 'Successfully submitted contact form.'
+          this.reset()
+        })
+        .catch(error => {
+          this.snackbar = true
+          this.snackbarText = 'There was an error submitting your form.'
+        })
+    }
+  },
 }
 </script>
